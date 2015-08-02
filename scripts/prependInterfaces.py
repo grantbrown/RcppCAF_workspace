1fbef3df8e2bb6f78e1af4e9774b6c7713ed4419
Grant Brown <grant.brown73@gmail.com>
2015-08-02 15:14:06 -0500

    file to add interface annotations to each c++ file

---
+++ scripts/prependInterfaces.py
@@ -0,0 +1,33 @@
+import os
+
+
+def processFiles(dirname):
+    savedPath = os.getcwd()
+    os.chdir(dirname)
+    err = 0
+    try:
+        files = [x for x in os.listdir("./") if (".cpp" in x)]
+        for f in files:
+            print(f)
+            f_handle = open(f, "r")
+            fdat = f_handle.readlines()
+            fdat.insert(0, "// [[Rcpp::interfaces(cpp)]]\n")
+            f_handle.close()
+            f_handle = open(f, "w")
+            f_handle.writelines(fdat)
+            f_handle.close()
+
+    except(e):
+        print(e)
+        err = 1
+        os.chdir(savedPath)
+    finally:
+        if (err == 0):
+            os.chdir(savedPath)
+        
+
+
+processFiles("../RcppCAF/src/libcaf_core")
+processFiles("../RcppCAF/src/libcaf_io")
+
+
